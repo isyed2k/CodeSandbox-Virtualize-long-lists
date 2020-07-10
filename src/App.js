c99e2da953e1aa87d3c6bf3705029ea980063d68
Irfan <isyed2k@users.noreply.github.com>
2020-07-10 12:12:15 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+
+import "./styles.css";
+
+const Row = ({ index, style }) => (
+  <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+    Row {index}
+  </div>
+);
+
+const App = () => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <List
+        className="List"
+        height={height}
+        itemCount={1000}
+        itemSize={35}
+        width={width}
+      >
+        {Row}
+      </List>
+    )}
+  </AutoSizer>
+);
+export default App;
